711283ad89af6841f531b8ac2f8aa50f709fccd4
Adebayo Adewusi <adewusiadebayo@gmail.com>
2025-05-03 19:15:05 +0100

    Make the buttons interactive

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import "./styles.css";
-import { greeting } from "./functions.js";
 
+const imgContainer = document.querySelectorAll('.img-container img');
+const mainImage = document.querySelector('.main-image');
+const nextBtn = document.querySelector('.next');
+const previousBtn = document.querySelector('.previous');
 
-greeting('Adebayo');
+const imgSources = Array.from(imgContainer).map(img => img.src);
+let currentIndex = 0;
+
+// Display main image
+mainImage.src = imgSources[currentIndex];
+
+// Next button event
+nextBtn.addEventListener('click', () => {
+  currentIndex = ( currentIndex + 1 ) % imgSources.length;
+  mainImage.src = imgSources[currentIndex];
+})
+
+// Previous button event
+previousBtn.addEventListener('click', () => {
+  currentIndex = ( currentIndex - 1 + imgSources.length) % imgContainer.length;
+  mainImage.src = imgSources[currentIndex];
+})
